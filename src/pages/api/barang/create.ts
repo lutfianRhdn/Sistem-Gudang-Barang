a5df33afd44485b647d9a0ee9435130af1bfdc05
AbdulAzisAlghifari <108967631+AbdulAzisAlghifari@users.noreply.github.com>
2023-08-17 00:50:43 +0700

    crud

---
+++ src/pages/api/barang/create.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@lib/database'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const { nama, jumlah,lokasi,kode,kategori_id } = req.body
+        console.log(req.body)
+        const product = await prisma.barang.create({
+          data: {
+            nama,
+            jumlah,
+            lokasi, 
+            kode,
+            kategori_id
+          }
+      })
+
+    return res.status(200).json({ product })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({  error })
+  }
+}
