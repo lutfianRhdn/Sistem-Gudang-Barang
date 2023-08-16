afff27aa7cd53faecb0263a3659dbbff803351d3
AbdulAzisAlghifari <108967631+AbdulAzisAlghifari@users.noreply.github.com>
2023-08-17 00:50:43 +0700

    crud

---
+++ src/pages/api/barang/delete/[id].ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@lib/database'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { id } = req.query
+
+    if (!id) {
+      return res.status(400).json({ error: 'ID parameter is required' })
+    }
+
+    const deletedProduct = await prisma.barang.delete({
+      where: {
+        id: parseInt(id as string, 10),
+      },
+    }) 
+
+    return res.status(200).json({ deletedProduct })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error })
+  }
+}
